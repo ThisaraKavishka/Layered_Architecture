df49721bf98e306b2e872f6c3f593ff954b4b3b2
thisara <thisarakavishka0@gmail.com>
2023-05-28 01:10:04 +0530

    Refactor OrderDAOImpl

--- src/superDAO/DAO/DAOImpl/OrderDAOImpl.java
+++ src/superDAO/DAO/DAOImpl/OrderDAOImpl.java
@@ -3,6 +3,7 @@ package superDAO.DAO.DAOImpl;
 import db.DBConnection;
 import model.OrderDTO;
 import superDAO.DAO.OrderDAO;
+import superDAO.SQLUtil;
 
 import java.sql.*;
 
@@ -10,27 +11,17 @@ public class OrderDAOImpl implements OrderDAO {
 
     @Override
     public String generateNewOID() throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        Statement stm = connection.createStatement();
-        ResultSet rst = stm.executeQuery("SELECT oid FROM `Orders` ORDER BY oid DESC LIMIT 1;");
+        ResultSet rst = SQLUtil.execute("SELECT oid FROM `Orders` ORDER BY oid DESC LIMIT 1;");
         return rst.next() ? String.format("OID-%03d", (Integer.parseInt(rst.getString("oid").replace("OID-", "")) + 1)) : "OID-001";
     }
 
     @Override
     public boolean exitOrder(String orderId) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement stm = connection.prepareStatement("SELECT oid FROM `Orders` WHERE oid=?");
-        stm.setString(1, orderId);
-        return stm.executeQuery().next();
+        return SQLUtil.execute("SELECT oid FROM `Orders` WHERE oid=?", orderId);
     }
 
     @Override
     public boolean saveOrder(OrderDTO dto) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement stm = connection.prepareStatement("INSERT INTO `Orders` (oid, date, customerID) VALUES (?,?,?)");
-        stm.setString(1, dto.getOrderId());
-        stm.setDate(2, Date.valueOf(dto.getOrderDate()));
-        stm.setString(3, dto.getCustomerId());
-        return stm.executeUpdate() > 0;
+        return SQLUtil.execute("INSERT INTO `Orders` (oid, date, customerID) VALUES (?,?,?)", dto.getOrderId(), Date.valueOf(dto.getOrderDate()), dto.getCustomerId());
     }
 }
